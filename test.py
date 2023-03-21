5df08b7c39d5b85576f61d503a4174e1d7727447
BOSUNGGONG <128451894+BOSUNGGONG@users.noreply.github.com>
2023-03-21 15:53:01 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,14 @@
+def giveone(a):
+	if a == 1:
+		return "1입니다"
+	elif a == 2:
+		return "2입니다"
+	elif a == 3:
+		return "3입니다"
+	else:
+		return
+
+print(giveone(1))
+print(giveone(2))
+print(giveone(3))
+print(giveone(4))
